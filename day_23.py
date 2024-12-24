7d9f9a5a9a203a26de63918f7bc4568e1953a290
NLSTAMAT <matthijs.vanstappen@sew-eurodrive.nlo>
2024-12-24 22:15:33 +0100

    day_23 complete

--- day_23.py
+++ day_23.py
@@ -1,41 +1,6 @@
-import copy
-
 from get_src import *
 
-sample = """kh-tc
-qp-kh
-de-cg
-ka-co
-yn-aq
-qp-ub
-cg-tb
-vc-aq
-tb-ka
-wh-tc
-yn-cg
-kh-ub
-ta-co
-de-co
-tc-td
-tb-wq
-wh-td
-ta-ka
-td-qp
-aq-cg
-wq-ub
-ub-vc
-de-ta
-wq-aq
-wq-vc
-wh-yn
-ka-de
-kh-ta
-co-tc
-wh-qp
-tb-vc
-td-yn"""
-inp = get(sample)
-
+inp = get()
 p= {}
 for node2 in inp.splitlines():
     a,b = node2.strip().split("-")
@@ -50,8 +15,7 @@ for node2 in inp.splitlines():
         p[b]=[a]
 
 ic = []
-for i in p:
-    print(i,p[i])
+
 for node1 in p:
     for node2 in p[node1]:
         for node3 in p[node2]:
@@ -60,25 +24,28 @@ for node1 in p:
                 t.sort()
                 ic.append("-".join(t))
 
-def find_myself(node,link,level,chain):
-    for subnode in p[link]:
-        if level>1 and subnode == node:
-            print(chain)
-            return chain
-        else:
-            if subnode not in chain:
-                chain2 = copy.deepcopy(chain)
-                chain2.append(subnode)
-                chain = find_myself(node,subnode, level + 1, chain2)
-    return chain
-
-for node1 in p:
-    print(node1)
-    print(find_myself(node1,node1,0,[node1]))
-
 a = 0
 for i in set(ic):
     if "-t" in i or i.startswith("t"):
         a+=1
-
 print(a)
+
+def find_loop(start_node, min_len=3):
+    t = set(p[start_node])
+    t.add(start_node)
+    for link in p[start_node]:
+        t2=set(p[link])
+        t2.add(link)
+        new_set = t.intersection(t2)
+        if len(new_set)>=min_len:
+            t=new_set
+    return t
+
+part_b = ""
+for i in p:
+    l = find_loop(i)
+    if len(l)>len(part_b):
+        part_b=l
+part_b = list(part_b)
+part_b.sort()
+print(",".join(part_b))
